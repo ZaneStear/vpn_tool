f17fb5bb278181490da0f94906478edb9a2ec335
Stear <1030349525@qq.com>
2023-02-11 10:02:45 +0800

    init

--- main.py
+++ main.py
@@ -4,19 +4,20 @@ from pathlib import Path
 
 
 def check_in():
-    har = json.loads(Path('mxwljsq.top.har').read_text())
-    r_body = har['log']['entries'][0]['request']
-    method = r_body['method']
-    url = r_body['url']
-    headers = r_body['headers']
-    cookies = r_body['cookies']
-    head = {}
-    for header in headers:
-        if str(header['name']).startswith(':'):
-            header['name'] = str(header['name']).lstrip(':')
-        head[header['name']] = header['value']
-    result = requests.post(url, headers=head)
-    print("签到结束: "+result.text)
+    # har = json.loads(Path('mxwljsq.top.har').read_text())
+    # r_body = har['log']['entries'][0]['request']
+    # method = r_body['method']
+    # url = r_body['url']
+    # headers = r_body['headers']
+    # cookies = r_body['cookies']
+    # head = {}
+    # for header in headers:
+    #     if str(header['name']).startswith(':'):
+    #         header['name'] = str(header['name']).lstrip(':')
+    #     head[header['name']] = header['value']
+    # result = requests.post(url, headers=head)
+    #print("签到结束: "+result.text)
+    print("签到结束")
 
 
 if __name__ == '__main__':
