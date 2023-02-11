cbd81ac0f66eea7cd9195a36cc24a11597ef9430
Stear <1030349525@qq.com>
2023-02-11 10:01:12 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import json
+import requests
+from pathlib import Path
+
+
+def check_in():
+    har = json.loads(Path('mxwljsq.top.har').read_text())
+    r_body = har['log']['entries'][0]['request']
+    method = r_body['method']
+    url = r_body['url']
+    headers = r_body['headers']
+    cookies = r_body['cookies']
+    head = {}
+    for header in headers:
+        if str(header['name']).startswith(':'):
+            header['name'] = str(header['name']).lstrip(':')
+        head[header['name']] = header['value']
+    result = requests.post(url, headers=head)
+    print("签到结束: "+result.text)
+
+
+if __name__ == '__main__':
+    check_in()
+    pass
